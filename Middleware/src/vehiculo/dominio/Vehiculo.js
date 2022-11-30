4f334370d216831d0d5f987e82a484d609f62bcf
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-29 20:34:00 -0600

    feat(vehiculo): registrar vehiculo

---
+++ Middleware/src/vehiculo/dominio/Vehiculo.js
@@ -0,0 +1,12 @@
+export class Vehiculo {
+  constructor(año, color, modelo, idMarca, numPlacas, numSerie, idPoliza, id) {
+    this.id = id;
+    this.año = año;
+    this.color = color;
+    this.modelo = modelo;
+    this.idMarca = idMarca;
+    this.numPlacas = numPlacas;
+    this.numSerie = numSerie;
+    this.idPoliza = idPoliza;
+  }
+}
---
+++ Middleware/src/vehiculo/infrestructura/persistencia/SqlServerVehiculoRepositorio.js
@@ -0,0 +1,90 @@
+import { TYPES } from "tedious";
+import { SqlExpressCriteriaParser } from "../../../compartido/infrestructura/utils/SqlExpressCriteriaParser.js";
+import { Vehiculo } from "../../dominio/Vehiculo.js";
+
+export class SqlServerVehiculoRepositorio {
+  constructor(conexion) {
+    this.conexion = conexion;
+  }
+
+  buscar = (criteria) =>
+    new Promise((resolve, reject) => {
+      let vehiculos = [];
+
+      let criteriaParser = new SqlExpressCriteriaParser(
+        [],
+        "Vehiculo",
+        criteria
+      );
+
+      let { consulta, parameters } = criteriaParser.parse();
+
+      let request = new Request(consulta, (err, rowCount) => {
+        if (err) {
+          console.log("Error VehiculoRepositorio: " + err);
+          reject(new Error("Error base de datos"));
+        } else {
+          console.log(rowCount + " rows");
+        }
+      });
+
+      parameters.forEach((parameter) => {
+        request.addParameter(
+          parameter.get("param"),
+          TYPES.VarChar,
+          parameter.get("value")
+        );
+      });
+
+      request.on("row", (columnas) => {
+        let vehiculo = new Vehiculo();
+        for (let name in columnas) {
+          vehiculo[name] = columnas[name].value;
+        }
+        vehiculos.push(vehiculo);
+      });
+
+      request.on("error", (error) => reject(error));
+      request.on("doneInProc", (rowCount, more, rows) => {
+        resolve(vehiculos);
+      });
+
+      this.conexion.execSql(request);
+    });
+
+  registrar = (vehiculo) =>
+    new Promise((resolve, reject) => {
+      let idVehiculo;
+      let consulta =
+        "INSERT INTO Vehiculo (año, color, idMarca, modelo, numPlacas, numSerie, idPoliza) " +
+        "VALUES (@value1, @value2, @value3, @value4, @value5, @value6, @value7); " +
+        "SELECT scope_identity();";
+
+      let request = new Request(consulta, (err) => {
+        if (err) {
+          console.log("Error VehiculoRepositorio: " + err);
+          reject(new Error("Error base de datos"));
+        }
+      });
+
+      request.addParameter("value1", TYPES.Int, vehiculo.año);
+      request.addParameter("value2", TYPES.VarChar, vehiculo.color);
+      request.addParameter("value3", TYPES.Int, vehiculo.idMarca);
+      request.addParameter("value4", TYPES.VarChar, vehiculo.modelo);
+      request.addParameter("value5", TYPES.VarChar, vehiculo.numPlacas);
+      request.addParameter("value6", TYPES.VarChar, vehiculo.numSerie);
+      request.addParameter("value7", TYPES.Int, vehiculo.idPoliza);
+
+      request.on("row", (columns) => {
+        idVehiculo = columns[0].value;
+        console.log("vehículo registrado con id: %d", idVehiculo);
+      });
+
+      request.on("error", (error) => reject(error));
+      request.on("doneInProc", () => {
+        resolve(idVehiculo);
+      });
+
+      this.conexion.execSql(request);
+    });
+}
