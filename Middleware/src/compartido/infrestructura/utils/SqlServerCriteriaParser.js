a5fc6889582d5ca70e99843f953567f614d8494b
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-12-03 00:55:00 -0600

    feat(criteria-parser): soporte para order, limit y offset

--- Middleware/src/compartido/infrestructura/utils/SqlServerCriteriaParser.js
+++ Middleware/src/compartido/infrestructura/utils/SqlServerCriteriaParser.js
@@ -37,6 +37,29 @@ export class SqlServerCriteriaParser {
       consulta += ` WHERE ${this.#createConditions()}`;
     }
 
+    let order = this._criteria.order;
+    let limit = this._criteria.limit
+    let offset = this._criteria.offset
+
+    if (order && !order.orderType.isNone()) {
+      if (order.orderType.isAsc())
+        consulta += ` ORDER BY ${order.orderBy.value} ASC`
+      else
+        consulta += ` ORDER BY ${order.orderBy.value} DESC`
+    } else if (limit || offset) {
+      consulta += ` ORDER BY (SELECT NULL)`
+    }
+
+    if (offset) {
+      consulta += ` OFFSET ${offset} ROWS`
+    } else if (limit) {
+      consulta += ` OFFSET 0 ROWS`
+    }
+
+    if (limit && limit > 0) {
+      consulta += ` FETCH NEXT ${limit} ROWS ONLY`
+    }
+
     consulta += ";";
 
     return consulta;
