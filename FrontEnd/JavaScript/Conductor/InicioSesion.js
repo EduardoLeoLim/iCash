d997c7892dae545e7ef154c512f7ddd3734ef67f
Eder Negrete <negretegarciaeib.cbtis13@gmail.com>
2022-12-10 12:45:12 -0600

    ref(FrontEnd): se modifico la peticion POST

--- FrontEnd/JavaScript/Conductor/InicioSesion.js
+++ FrontEnd/JavaScript/Conductor/InicioSesion.js
@@ -3,32 +3,42 @@ function iniciarSesion() {
     var usuario = document.getElementById('usuario').value;
     var contrasenia = document.getElementById('contrasenia').value;
     if (usuario != "" && contrasenia != "") {
-        location.href = "../../HTML/Conductor/ModificarCuenta.html";
+        comprobarCredenciales(usuario, contrasenia)
+        .then(function(){
+            location.href = "../../HTML/Conductor/ModificarCuenta.html";
+        })
+        .catch(function (error){{
+            alert("Credenciales invalidas");
+        }});
     }else{
         alert("Campos incompletos");
     }
 }
 
-/*
+function comprobarCredenciales(usuario, contrasenia) {
+    return new Promise(function(res, rej){
+        axios.post(URL_BASE+'/conductor/login', {
+            nombreUsuario: usuario,
+            claveAcceso: contrasenia
+          })
+          .then(function (response) {
+            agregarValoresStorage(response)
+            res()
+          })
+          .catch(function (error) {
+            rej()
+          });
+    })
+}
 
-function agregarValoresStorage(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 & this.status == 200){
-            const header = new Headers();
-            const token = header.get('authorization');
-            const nombreUsuario
-            sessionStorage.setItem("token", token);
-            sessionStorage.setItem("nombreUsuario", nombreUsuario);
-            sessionStorage.setItem("idUsuario", idUsuario);
-        }
-    };
-    xhttp.open("POST", "", true);
-    xhttp.send();
+
+function agregarValoresStorage(response){
+    sessionStorage.setItem("token", response.headers.authorization)
+    sessionStorage.setItem("nombreUsuario", response.data.nombreUsuario)
+    sessionStorage.setItem("contrasena", response.data.claveAcceso)
+    sessionStorage.setItem("idUsuario", response.data.id)
 }
 
 window.onload = function(){
-    existenValoresStorage();
+    document.getElementById("botonIniciarSesion").onclick = iniciarSesion;
 };
-
-*/
