62c8ea949268bb408a8824deb29ee7e1e4ff2485
Eder Negrete <negretegarciaeib.cbtis13@gmail.com>
2022-12-10 17:23:42 -0600

    feat(Usuario): consultar usuario por id

---
+++ Middleware/src/usuario/aplicacion/ConsultarUsuarioPorId.js
@@ -0,0 +1,28 @@
+import { CriteriaBuilder } from "../../compartido/dominio/criteria/CriteriaBuilder.js";
+import  ResourceNotFoundError  from "../../compartido/aplicacion/excepciones/ResourceNotFoundError.js";
+
+export default class ConsultarUsuarioPorId {
+  constructor(repositorio) {
+    this._repositorio = repositorio;
+  }
+
+  buscarUsuario = (idUsuario) =>
+    new Promise((resolve, reject) => {
+      let criteria = new CriteriaBuilder()
+        .orderAsc("id")
+        .equal("id", idUsuario)
+        .obligatory()
+        .build();
+
+      this._repositorio
+        .buscar(criteria)
+        .then((usuarios) => {
+          if (usuarios.length > 0) {
+            resolve(usuarios[0]);
+          } else {
+            reject(new ResourceNotFoundError("Usuario no encontrado"));
+          }
+        })
+        .catch((error) => reject(error));
+    });
+}
