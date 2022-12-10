0ec67b434f893a43ba71246f383a1dc77c2853cb
Eder Negrete <negretegarciaeib.cbtis13@gmail.com>
2022-12-10 17:02:37 -0600

    refactor(ReporteSiniestro): se agregron nuevos atributos pertenecientes a CONDUCTOR

--- Middleware/src/reportesiniestro/dominio/ReporteSiniestro.js
+++ Middleware/src/reportesiniestro/dominio/ReporteSiniestro.js
@@ -1,10 +1,25 @@
 export default class ReporteSiniestro {
-  constructor(nombre, estatus, fecha, latitud, longitud, id) {
+  constructor(
+    nombre,
+    estatus,
+    fecha,
+    latitud,
+    longitud,
+    nombreConductor,
+    apellidoPaterno,
+    apellidoMaterno,
+    idConductor,
+    id
+  ) {
     this.nombre = nombre;
     this.estatus = estatus;
     this.fecha = fecha;
     this.latitud = latitud;
     this.longitud = longitud;
+    this.conductor = {
+      id: idConductor,
+      nombre: nombreConductor + " " + apellidoPaterno + " " + apellidoMaterno,
+    };
     this.id = id;
   }
 }
