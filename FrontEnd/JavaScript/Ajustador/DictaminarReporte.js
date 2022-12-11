c011942e1ab734cb778b0ca7807e9f96b818c0a4
raul8500 <lopezjo299@gmail.com>
2022-12-11 17:11:04 -0600

    feat: Se actualizo el frontend

--- FrontEnd/JavaScript/Ajustador/DictaminarReporte.js
+++ FrontEnd/JavaScript/Ajustador/DictaminarReporte.js
@@ -1,9 +1,28 @@
 
 function dictaminarReporte (){
     if(comprobarValoresVacios()){
-        alert("Paso");
+        //se quita cuando blas acabe
+        sessionStorage.setItem("idReporteSiniestro",2);
+        const idReportesiniestro = sessionStorage.getItem("idReporteSiniestro");
+        var descripcion = document.getElementById("txaDescripcion").value;
+        var date = document.getElementById("inputDate").value;
+
+        return new Promise(function(res, rej){
+            axios.post(URL_BASE+'/ajustador/'+idReportesiniestro+'/dictaminar', {
+                fecha: date,
+                descripcion: descripcion,
+                idReporteSiniestro: idReportesiniestro
+              })
+              .then(function (response) {
+                res()
+              })
+              .catch(function (error) {
+                rej(error)
+              });
+        })
     }
 }
+
 
 function comprobarValoresVacios(){
     var descripcion = document.getElementById("txaDescripcion").value;
@@ -19,6 +38,6 @@ function comprobarValoresVacios(){
 
 window.onload = function(){
     //Descomentar linea 43 cuando se tenga login Ajustador
-    //existenValoresStorage("../../HTML/Conductor/InicioSesion.html")
+    existenValoresStorage("../../HTML/Ajustador/LoginAjustador.html")
     document.getElementById("btnDictaminar").onclick = dictaminarReporte;
 };
