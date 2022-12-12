7d2ab9392c8f6884246123cbc28bd884c68c1a5b
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-12-12 14:40:49 -0600

    feat(involucrado): repositorio de involucrados

--- Middleware/src/involucrado/infrestructura/persistencia/SqlServerInvolucradoRepositorio.js
+++ Middleware/src/involucrado/infrestructura/persistencia/SqlServerInvolucradoRepositorio.js
@@ -1,6 +1,7 @@
-import { Request } from "tedious";
-import { SqlServerCriteriaParser } from "../../../compartido/infrestructura/utils/SqlServerCriteriaParser.js";
+import { Request, TYPES } from "tedious";
+import SqlServerCriteriaParser from "../../../compartido/infrestructura/utils/SqlServerCriteriaParser.js";
 import Involucrado from "../../dominio/Involucrado.js";
+import ApplicationError from "../../../compartido/aplicacion/excepciones/ApplicationError.js";
 
 export default class SqlServerInvolucradoRepositorio {
   constructor(conexion) {
@@ -22,7 +23,7 @@ export default class SqlServerInvolucradoRepositorio {
       let request = new Request(consulta, (err, rowCount) => {
         if (err) {
           console.log("Error InvolucradoRepositorio: " + err);
-          reject(new Error("Error base de datos"));
+          reject(new ApplicationError(500, "Error base de datos"));
         } else {
           console.log(rowCount + " filas");
           resolve(involucrados);
@@ -43,6 +44,34 @@ export default class SqlServerInvolucradoRepositorio {
           involucrado[name] = columnas[name].value;
         }
         involucrados.push(involucrado);
+      });
+
+      this.conexion.execSql(request);
+    });
+
+  registrar = (involucrado) =>
+    new Promise((resolve, reject) => {
+      let idInvolucrado;
+      let consulta =
+        "INSERT INTO Involucrado (nombre, idReporteSiniestro, idVehiculo) " +
+        "VALUES (@value1, @value2, @value3);" +
+        "SELECT scope_identity();";
+
+      let request = new Request(consulta, (err) => {
+        if (err) {
+          console.log("Error InvolucradoRepositorio: " + err);
+          reject(new ApplicationError(500, "Error base de datos"));
+        } else {
+          resolve(idInvolucrado);
+        }
+      });
+
+      request.addParameter("value1", TYPES.VarChar, involucrado.nombre);
+      request.addParameter("value2", TYPES.Int, involucrado.idReporteSiniestro);
+      request.addParameter("value3", TYPES.Int, involucrado.idVehiculo);
+
+      request.on("row", (columnas) => {
+        idInvolucrado = columnas[0].value;
       });
 
       this.conexion.execSql(request);
