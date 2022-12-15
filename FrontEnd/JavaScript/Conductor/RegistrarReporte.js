a4303c3cf3b43117e54751f3bd4ad6cc1ad1d72e
raul8500 <lopezjo299@gmail.com>
2022-12-15 06:33:07 -0600

    feat(frontend): registrar reporte

--- FrontEnd/JavaScript/Conductor/RegistrarReporte.js
+++ FrontEnd/JavaScript/Conductor/RegistrarReporte.js
@@ -1,18 +1,15 @@
 
 var involucrados = [];
-
 var entidadesFederativas = [];
 var municipios = [];
-
 var polizas = [];
-
-var marcas =  [{id:1},{id:2},{id:3}];
-
+var marcas =  [];
 var nombreInvolucrado
 var marcaTxt
 var modeloTxt
 var colorTxt
 var numPlacasTxt 
+var añoAuto
 var checkBoxDatosVehiculo
 var btnAgregar
 var numSerieTxt
@@ -23,8 +20,12 @@ var longitud
 var latitudTxt
 var longitudTxt
 var tituloTxt
-
+var cbMarcas
 var idConductor
+var token
+let municipio
+let estado
+let selectPoliza
 
 function validaCheckbox()
 {
@@ -34,6 +35,7 @@ function validaCheckbox()
   colorTxt.disabled = !checked
   numPlacasTxt.disabled = !checked
   numSerieTxt.disabled = !checked
+  añoAuto.disabled = !checked
 }
 
 
@@ -51,7 +53,7 @@ function agregarInvolucrado (even){
     if(checked){
         if(marcaTxt.value.trim().length == 0 || modeloTxt.value.trim().length == 0 || 
             colorTxt.value.trim().length == 0 || numPlacasTxt.value.trim().length == 0 ||
-            numSerieTxt.value.trim().length == 0 ){
+            numSerieTxt.value.trim().length == 0 || añoAuto.value.trim().length == 0){
 
             return
         }
@@ -68,12 +70,12 @@ function agregarInvolucrado (even){
 
 
     if(checked){
-
-        vehiculo.idMarca = marcas[marcaTxt.value].id
+        vehiculo.idMarca = parseInt(cbMarcas.value)
         vehiculo.modelo = modeloTxt.value.trim()
         vehiculo.numPlacas = numPlacasTxt.value.trim()
         vehiculo.color = colorTxt.value.trim()
         vehiculo.numSerie = numSerieTxt.value.trim()
+        vehiculo.año = parseInt(añoAuto.value.trim())
         involucrado.vehiculo = vehiculo 
     }
     involucrados.push(involucrado)
@@ -92,19 +94,28 @@ function recargarTabla(){
 
 
     involucrados.forEach(involucrado => {
-        alert(involucrado.nombre)
+        let nombreMarca = involucrado.vehiculo? marcas.find(marca => marca.id = involucrado.vehiculo.idMarca).nombre:""
+        let modelo = involucrado.vehiculo? involucrado.vehiculo.modelo:""
+        let color = involucrado.vehiculo? involucrado.vehiculo.color:""
+        let numPlacas = involucrado.vehiculo? involucrado.vehiculo.numPlacas:""
+        let numSerie = involucrado.vehiculo? involucrado.vehiculo.numSerie:""
+        let año = involucrado.vehiculo? involucrado.vehiculo.año:""
+
         let template = `
                         <tr>
                             <td>${involucrado.nombre}</td>
-                            <td>${involucrado.vehiculo.idMarca}</td> 
-                            <td>${involucrado.vehiculo.modelo}</td>
-                            <td>${involucrado.vehiculo.color}</td>
-                            <td>${involucrado.vehiculo.numPlacas}</td>
-                            <td>${involucrado.vehiculo.numSerie}</td>
+                            <td>${nombreMarca}</td> 
+                            <td>${modelo}</td>
+                            <td>${color}</td>
+                            <td>${numPlacas}</td>
+                            <td>${numSerie}</td>
+                            <td>${año}</td>
+
                         </tr>`;
         tabla.innerHTML += template;
     })
-} 
+}
+
 
 function removerInvolucrados (even){
     even.preventDefault()
@@ -119,6 +130,7 @@ function limpiarCampos(){
     colorTxt.value = ""
     numPlacasTxt.value = ""
     numSerieTxt.value = ""
+    añoAuto.value = ""
 }
 
 function calcularUbicacion (even){
@@ -145,6 +157,19 @@ function accesoUbicacionDenegado( err){
 }
 
 
+function cargarMarcas(){
+    return new Promise(function(res, rej){
+        axios.get(URL_BASE+'/marcas')
+          .then(function (response) {
+            res(response.data)
+          })
+          .catch(function (error) {
+            rej(error)
+          });
+    })
+}
+
+
 function cargarPolisas(idConductor){
     return new Promise(function(res, rej){
         axios.get(URL_BASE+'/conductores/'+idConductor+"/polizas")
@@ -157,24 +182,6 @@ function cargarPolisas(idConductor){
     })
 }
 
-function cargarPolisasCb (listaPolizas){
-
-    let select = document.getElementById("polizaConductor");
-    removeOptions(select);
-    let placeHolder = document.createElement("option");
-    placeHolder.textContent = "Selecciona una poliza"
-    placeHolder.value = null
-
-    select.appendChild(placeHolder);
-
-    for(let i = 0; i < listaPolizas.length; i++) {
-        let opt = listaPolizas[i];
-        let el = document.createElement("option");
-        el.textContent = opt.nombre;
-        el.value = opt;
-        select.appendChild(el);
-    }
-}
 
 function cargarComboBoxMunicipicios(claveEntidadFederativa){
     return new Promise(function(res, rej){
@@ -201,6 +208,24 @@ function cargarEntidadesFederativas(){
     })
 }
 
+function cargarPolisasCb (listaPolizas){
+
+    selectPoliza = document.getElementById("polizaConductor");
+    removeOptions(selectPoliza);
+    let placeHolder = document.createElement("option");
+    placeHolder.textContent = "Selecciona una poliza"
+    placeHolder.value = null
+
+    selectPoliza.appendChild(placeHolder);
+
+    for(let i = 0; i < listaPolizas.length; i++) {
+        let opt = listaPolizas[i];
+        let el = document.createElement("option");
+        el.textContent = opt.nombre;
+        el.value = opt.id;
+        selectPoliza.appendChild(el);
+    }
+}
 
 function cargarEntidadesFederativasCb (entidadesFederativas){
 
@@ -223,8 +248,47 @@ function cargarEntidadesFederativasCb (entidadesFederativas){
     select.onchange = filtrarMunicipios
 }
 
+
+function cargarMunicipiosCb (municipios){
+
+    municipio = document.getElementById("cbMunicipio");
+    removeOptions(municipio);
+    let placeHolder = document.createElement("option");
+    placeHolder.textContent = "Selecciona un municipio"
+    placeHolder.value = null
+
+
+    municipio.appendChild(placeHolder);
+
+    for(let i = 0; i < municipios.length; i++) {
+        let opt = municipios[i];
+        let el = document.createElement("option");
+        el.textContent = opt.nombre;
+        el.value = opt.clave;
+        municipio.appendChild(el);
+    }
+}
+
+function cargarMarcasCb (marcas){
+    cbMarcas = document.getElementById("cbMarcaInvolucrado");
+    removeOptions(cbMarcas);
+    let placeHolder = document.createElement("option");
+    placeHolder.textContent = "Selecciona una marca"
+    placeHolder.value = null
+
+    cbMarcas.appendChild(placeHolder);
+
+    for(let i = 0; i < marcas.length; i++) {
+        let opt = marcas[i];
+        let el = document.createElement("option");
+        el.textContent = opt.nombre;
+        el.value = opt.id;
+        cbMarcas.appendChild(el);
+    }
+}
+
 function filtrarMunicipios(){
-    let estado = document.getElementById("cbEntidadFederativa");
+    estado = document.getElementById("cbEntidadFederativa");
     let clave = estado.value
 
     if(clave && clave != null ){
@@ -237,26 +301,6 @@ function filtrarMunicipios(){
     }
 }
 
-function cargarMunicipiosCb (municipios){
-
-    let select = document.getElementById("cbMunicipio");
-    removeOptions(select);
-    let placeHolder = document.createElement("option");
-    placeHolder.textContent = "Selecciona un municipio"
-    placeHolder.value = null
-
-
-    select.appendChild(placeHolder);
-
-    for(let i = 0; i < municipios.length; i++) {
-        let opt = municipios[i];
-        let el = document.createElement("option");
-        el.textContent = opt.nombre;
-        el.value = opt;
-        select.appendChild(el);
-    }
-}
-
 function removeOptions(selectElement) {
     var i, L = selectElement.options.length - 1;
     for(i = L; i >= 0; i--) {
@@ -264,7 +308,45 @@ function removeOptions(selectElement) {
     }
  }
 
+function buttonregistrar (even){
+tituloTxt
+    const inputF = document.getElementById("imagenesIn")
+    const archivos = inputF.files
+
+    even.preventDefault()
+    const horaAcciedente = document.getElementById("horaAccidente").value;
+    const formData = new FormData();
+
+    formData.append("nombre", tituloTxt.value  )
+    formData.append("horaAccidente", horaAcciedente)
+    formData.append("claveMunicipio", municipio.value)
+    formData.append("claveEntidadFederativa", estado.value)
+    formData.append("latitud", latitudTxt.value)
+    formData.append("longitud", longitudTxt.value)
+    formData.append("idPoliza",selectPoliza.value )
+    formData.append("idConductor", idConductor)
+    formData.append("involucrados", JSON.stringify(involucrados))
+   
     
+    for (let index = 0; index < archivos.length; index++) {
+        formData.append("imagen" + (index + 1), archivos[index])
+    }
+   
+    axios.post(URL_BASE+"/conductores/reportesSiniestro", formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': token
+        }
+    }).then((resultado) => {
+        alert("Reporte registrado")
+        location.reload()
+    }).catch((error) => {
+        console.log(error)
+        alert("Erro no se puedo registrar")
+    })
+}
+
+
 
 window.onload = function(){
 
@@ -277,6 +359,7 @@ window.onload = function(){
     colorTxt = document.getElementById("colorInvolucrado")
     numPlacasTxt = document.getElementById("numPlacasInvolucrado")
     numSerieTxt = document.getElementById("numSerieInvolucrado")
+    añoAuto = document.getElementById("añoInvolucrado") 
     tabla = document.getElementById("tableInvolucrado")
     btnAgregar = document.getElementById("btnAgregarInvolucrado")
     btnAgregar.onclick = agregarInvolucrado
@@ -285,6 +368,7 @@ window.onload = function(){
     btnRemover = document.getElementById("btnRemoverInvolucrados")
     btnRemover.onclick = removerInvolucrados
 
+    token = sessionStorage.getItem("token")
     tituloTxt = document.getElementById("tituloReporteText")
 
     latitudTxt = document.getElementById("latitudText")
@@ -294,13 +378,17 @@ window.onload = function(){
     btnUbicacion.onclick = calcularUbicacion
 
 
+    btnRegistrar = document.getElementById("btnRegistrar")
+    btnRegistrar.onclick = buttonregistrar
+
+
     marcaTxt.disabled = true
     modeloTxt.disabled = true
     colorTxt.disabled = true
     numPlacasTxt.disabled = true
     numSerieTxt.disabled = true
     checkBoxDatosVehiculo.checked = false
-
+    añoAuto.disabled = true
 
     cargarEntidadesFederativas().then(listaEntidadesFederativas => {
         entidadesFederativas = listaEntidadesFederativas
@@ -314,5 +402,12 @@ window.onload = function(){
         cargarPolisasCb(polizas)
     } ).catch ((err)=> 
         alert("error al cargar polizas")
+    )
+
+    cargarMarcas().then(listamarcas => {
+        marcas = listamarcas
+        cargarMarcasCb(marcas)
+    } ).catch ((err)=> 
+        alert("error al cargar marcas")
     )
 }
